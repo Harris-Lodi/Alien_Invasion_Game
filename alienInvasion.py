5a2a0f4631868e51ca7e7d2f67021cb890c65823
Harris-Lodi <harrislodi90s@gmail.com>
2020-07-01 02:34:34 -0400

    Added more functioanlity to settings and ship update
    Added more stuff, moving to bullet.py next

--- alienInvasion.py
+++ alienInvasion.py
@@ -3,31 +3,31 @@ from settings import Settings
 from ship import Ship
 import pygame
 
-# Overall class to manage game assets and behavior, cont on page 238
+# Overall class to manage game assets and behavior, cont on page 247
 class alienInvasion:
 
     # init function to run upon launch
     def __init__(self):
+
         # initialize the game and game creation resources
         pygame.init()
 
         # window, import settings from settings.py script
         self.settings = Settings()
 
-        # apply window dimensions
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        # set game to full screen
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # change settings dimensions to match screen size
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height 
 
         # set title of window
         pygame.display.set_caption("Alien Invasion")
 
         # init ship
         self.ship = Ship(self)
-
-        # set background color
-        self.bg_color = (0, 0, 255)
     
+    # function to invoke the main game loop for in game updates
     def runGame(self):
 
         # start main game loop
@@ -35,6 +35,12 @@ class alienInvasion:
 
             # check for input events while loop
             self._check_events()
+
+            # ship update function running in game loop
+            self.ship.update()
+
+            # debug the ship position for testing
+            # print(self.ship.x)
             
             # update screen while looping
             self._update_screen()
@@ -42,10 +48,39 @@ class alienInvasion:
     # function to check for keypresses and mouse events
     def _check_events(self):
 
-            # watch for keyboard and mouse events for exit window
+            # watch for keyboard and mouse events 
             for event in pygame.event.get():
+                
+                # for closing window 
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+                # when key on keyboard is pressed down
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+
+                # when key on key board is released
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+
+    # function to respond to key presses
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            # set ship movement flag for right movement true
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # same but for left movement
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            # close game on pressing 'q'
+            sys.exit()
+
+    # function to respond to key releases
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     # function to update screen in loop
     def _update_screen(self):
--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame
 # a class to manage the ship
 class Ship:
 
-    # init the ship and set starting position
+    # init the ship and set starting position, and settings
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get it's box
@@ -14,7 +15,28 @@ class Ship:
 
         # start each new ship at the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value for the ship's horizontal positions
+        self.x = float(self.rect.x)
+
+        # movement flag
+        self.moving_right = False
+        self.moving_left = False
+    
+    # update the ship's position based on movement flag
+    def update(self):
+
+        # update the ship's x value, and limit it between screen boundaries
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
+        
+        # update rect object from self.x
+        self.x = self.rect.x
     
     # draw the ship at it's curent location
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+    
+    
